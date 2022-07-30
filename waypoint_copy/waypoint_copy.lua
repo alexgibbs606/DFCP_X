e1691b88e4d927a6aa356fd719e7de3835980f85
nadel <crjohannsen@gmail.com>
2022-07-30 10:57:17 -0700

    added instructions, simplified "args", added support for red coalition

--- waypoint_copy/waypoint_copy.lua
+++ waypoint_copy/waypoint_copy.lua
@@ -1,44 +1,46 @@
-
-
--- to make this work
--- extract the *.miz/mission file
--- rename mission to mission.lua
--- make the change
--- write out the file
--- repack into a *.miz file
+-- INSTRUCTIONS
+--  1. Create a backup copy of your miz file
+--  2. Open your miz file in 7zip "open archive"
+--  3. Drag the "mission" file out of the zip, and into a folder
+--  4. Update the ARGS below these instructions:
+--       - working_directory should match the file path where you put the mission file
+--       - choose coalition, and flight names in your mission
+--  5. Run the lua script
+--  6. Drag your output file back into 7zip
+--  7. Open the miz in DCS, it *should* work :)
 
 
 --ARGS
+local working_directory = [[C:\Users\Chris\Saved Games\DCS\Missions\scripts\DFCP-ME\waypoint_copy\]] -- make sure this ends with "\"
+-- choose the coalition/flights to copy
+local src = "IVAN" -- group name to copy waypoints from
+local dst_all = {"ORPHAN", "ORPHAN-FR", "PIRATE", "PATCH", "PATCH_AUS"} -- group names to copy waypoints to
+-- END OF ARGS
+
+
+local mission_file_name = "mission" -- file must be present in the above working_directory folder
+local out_file_name = "mission.updated.lua" -- file will be created in the above working_directory folder
+
 -- TODO: arg parser, python wrapper?
-local mission_file_path = "C:\\Users\\Chris\\Saved Games\\DCS\\Missions\\scripts\\DFCP-ME\\waypoint_copy\\mission.lua"
-local out_file_path = "C:\\Users\\Chris\\Saved Games\\DCS\\Missions\\scripts\\DFCP-ME\\waypoint_copy\\mission.updated.lua"
-local coalition = "blue" -- or red
-local src = "PIRATE" -- group name to copy waypoints from
-local dst_all = {"ORPHAN", "ORPHAN-FR", "PIRATE", "PATCH", "PATCH_AUS"} -- group name to copy waypoints to
+
+
+-- build a helper lookup table for flights we'll modify
+local dst_lookup = {}
+for i, v in ipairs(dst_all) do
+    dst_lookup[v] = true
+end
 
 
 
-
-
-
---- read in the mission file
-local handle = io.open(mission_file_path,'rb')
-local raw_data_str  = handle:read("*a")
-handle:close()
-raw_data_lua = raw_data_str .. "\n return mission" 
-
-local mission_data = load(raw_data_lua)()
-
+-- main function to dump the updated mission.lua
 function serialize(filepath, o)
     file = io.open(filepath, "w+")
-    -- file:write("mission = {\n")
     file:write("mission = ")
     serialize_inner(file, o, "  ")
-    -- file:write("}")
     io.close(file)
 end
 
--- function to write out the data
+-- helper function to write out the mission.lua contents in "readable" form
 function serialize_inner (file, o, indent)
     if type(o) == "number" then
         file:write(o)
@@ -68,33 +70,45 @@ function serialize_inner (file, o, indent)
     end
 end
 
-local copy_source = nil
-local spinnybois = {} -- helicopters [country_id, group_number, name, route]
-local pointybois = {} -- planes
+
+-- read in the mission file
+local mission_file_path = working_directory .. mission_file_name
+local handle = io.open(mission_file_path,'rb')
+local raw_data_str  = handle:read("*a")
+handle:close()
+local raw_data_lua = raw_data_str .. "\n return mission" 
+local mission_data = load(raw_data_lua)()
 
 
--- read in the waypoint data
-for country_id, unit_table in ipairs(mission_data["coalition"][coalition]["country"]) do
-    -- if the country has spinnybois, go get all their waypoints
-    if unit_table["helicopter"] ~= nil then
-        print(unit_table["name"] .. "(" ..country_id ..") helicopters:")
-        for group_index, group_data in ipairs(unit_table["helicopter"]["group"]) do
-            print(group_index .. " - " .. group_data["name"])
-            spinnybois[#spinnybois+1] = {country_id, group_index, group_data["name"], group_data["route"]}
-            if group_data["name"] == src then
-                copy_source = group_data["route"]
+
+-- find the waypoint data for *all* aircraft
+local copy_source = nil -- the waypoints we'll copy into the target flights
+local spinnybois = {} -- helicopters [coalition, country_id, group_number, name, route]
+local pointybois = {} -- planes [coalition, country_id, group_number, name, route]
+
+for i, coalition in ipairs{"red", "blue"} do
+    for country_id, unit_table in ipairs(mission_data["coalition"][coalition]["country"]) do -- for each country on red/blue
+        -- if the country has spinnybois, go get all their waypoints
+        if unit_table["helicopter"] ~= nil then
+            print(unit_table["name"] .. "(" ..country_id ..") helicopters:")
+            for group_index, group_data in ipairs(unit_table["helicopter"]["group"]) do
+                print(group_index .. " - " .. group_data["name"])
+                spinnybois[#spinnybois+1] = {coalition, country_id, group_index, group_data["name"], group_data["route"]}
+                if group_data["name"] == src then
+                    copy_source = group_data["route"]
+                end
             end
         end
-    end
 
-    -- if the country has pointybois, go get all their waypoints
-    if unit_table["plane"] ~= nil then
-        print(unit_table["name"] .. "(" ..country_id ..") planes:")
-        for group_index, group_data in ipairs(unit_table["plane"]["group"]) do
-            print(group_index .. " - " .. group_data["name"])
-            pointybois[#pointybois+1] = {country_id, group_index, group_data["name"], group_data["route"]}
-            if group_data["name"] == src then
-                copy_source = group_data["route"]
+        -- if the country has pointybois, go get all their waypoints
+        if unit_table["plane"] ~= nil then
+            print(unit_table["name"] .. "(" ..country_id ..") planes:")
+            for group_index, group_data in ipairs(unit_table["plane"]["group"]) do
+                print(group_index .. " - " .. group_data["name"])
+                pointybois[#pointybois+1] = {coalition, country_id, group_index, group_data["name"], group_data["route"]}
+                if group_data["name"] == src then
+                    copy_source = group_data["route"]
+                end
             end
         end
     end
@@ -105,42 +119,41 @@ assert(copy_source, "did not find '" .. src .. "' in the mission.")
 
 
 
--- build a lookup table for flights we'll modify
-local dst_lookup = {}
-for i, v in ipairs(dst_all) do
-    dst_lookup[v] = true
-end
 
-
-
--- modify the spinnybois 
+-- update the spinnybois waypoints
 for index, data in ipairs(spinnybois) do
-    local country_id = data[1]
-    local group_index = data[2]
-    local group_name = data[3]
+    local coalition = data[1]
+    local country_id = data[2]
+    local group_index = data[3]
+    local group_name = data[4]
 
-    if dst_lookup[group_name] then
+    if dst_lookup[group_name] then -- only update groups that we were asked to
         for i, idata in ipairs(copy_source["points"]) do
             if i > 1 then -- skip wp0
+                -- write the waypoint to the miz file stored in memory
                 mission_data["coalition"][coalition]["country"][country_id]["helicopter"]["group"][group_index]["route"]["points"][i] = idata
             end
         end
     end
 end
 
--- modify the pointybois
+-- update the pointybois waypoints
 for index, data in ipairs(pointybois) do
-    local country_id = data[1]
-    local group_index = data[2]
-    local group_name = data[3]
+    local coalition = data[1]
+    local country_id = data[2]
+    local group_index = data[3]
+    local group_name = data[4]
 
-    if dst_lookup[group_name] then
+    if dst_lookup[group_name] then -- only update groups that we were asked to
         for i, idata in ipairs(copy_source["points"]) do
             if i > 1 then -- skip wp0
+                -- write the waypoint to the miz file stored in memory
                 mission_data["coalition"][coalition]["country"][country_id]["plane"]["group"][group_index]["route"]["points"][i] = idata
             end
         end
     end
 end
 
+-- write the miz file in memory out to disk
+local out_file_path = working_directory .. out_file_name
 serialize(out_file_path, mission_data)
