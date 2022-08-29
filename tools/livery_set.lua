b866819feae4f21093583e4fe32bed182ad81ad9
nadel <crjohannsen@gmail.com>
2022-08-29 15:27:02 -0700

    basic tool for setting all liveries in a mission

---
+++ tools/livery_set.lua
@@ -0,0 +1,106 @@
+-- holy shit this is a hacky relative import. there must be a better way??? this just works for testing the file directly
+-- local filename = "livery_set.lua" -- fragile, but I hate regex
+-- local module_path = arg[0] -- only works if we're executing *this* file, but for tools this should be ok
+-- module_path = string.sub(module_path, 0, string.len(module_path)-string.len(filename))
+-- package.path = module_path .. "/lib/?.lua;" .. package.path
+
+
+
+--ARGS: ./lua54.exe livery_set.lua "<full_path_to_file_to_modify>" "<full path to folder this file is in>" [desert|winter]
+local LIB_PATH = arg[1] -- TODO: how can we find our library path in lua and avoid this ugly hack??
+local FILEPATH = arg[2]
+local LIVERY = arg[3]
+
+-- print("###")
+-- print(LIB_PATH)  
+-- print(FILEPATH)
+-- print(LIVERY)
+-- print("###")
+package.path = LIB_PATH .. "/?.lua;" .. package.path
+
+require("mission_io")
+
+-- Warning: unit names AND skin names don't match what you see in the editor. You have to open the mission file to find these.
+local desert = {} -- best "desert" skin for each known unit
+desert["Leclerc"] = "desert"
+desert["Ural-375"] = "desert"
+desert["BMP-1"] = "desert"
+desert["BTR-80"] = "desert"
+desert["M1043 HMMWV Armament"] = "desert"
+desert["Hummer"] = "desert"
+desert["SA-11 Buk SR 9S18M1"] = "desert"
+desert["SA-11 Buk CC 9S470M1"] = "desert"
+desert["SA-11 Buk LN 9A310M1"] = "desert"
+desert["S-60_Type59_Artillery"] = "SYR"
+
+
+desert["M1097 Avenger"] = "default"
+desert["Cobra"] = "default"
+desert["M 818"] = "default"
+
+
+local winter = {} -- best "winter" skin for each known unit
+winter["Ural-375"] = "winter"
+winter["BMP-1"] = "winter"
+winter["BTR-80"] = "winter"
+winter["M1043 HMMWV Armament"] = "winter"
+winter["Hummer"] = "winter"
+winter["M 818"] = "winter"
+winter["SA-11 Buk SR 9S18M1"] = "winter"
+winter["SA-11 Buk CC 9S470M1"] = "winter"
+
+winter["M1097 Avenger"] = "default"
+winter["Cobra"] = "default"
+winter["SA-11 Buk LN 9A310M1"] = "default"
+
+
+
+local lookup_tables = {}
+lookup_tables["desert"] = desert
+lookup_tables["winter"] = winter
+local chosen_table = lookup_tables[LIVERY]
+assert(chosen_table ~= nil)
+
+mission_data = mission_io.read_mission(FILEPATH)
+
+
+
+
+for i, coalition in ipairs{"red", "blue"} do
+    local unknown_list = {}
+    local unavailable_list = {}
+    for country_id, unit_table in ipairs(mission_data["coalition"][coalition]["country"]) do -- for each country on red/blue
+        if unit_table["vehicle"] ~= nil then
+            for group_index, group_data in ipairs(unit_table["vehicle"]["group"]) do
+                for unit_index, unit_data in ipairs (group_data["units"]) do
+                    local unit_type = unit_data["type"]
+                    local unit_name = unit_data["name"]
+                    local unit_livery = "default"
+                    if unit_data["livery_id"] ~= nil then
+                        unit_livery = unit_data["livery_id"]
+                    end
+                    if chosen_table[unit_type] == nil then -- unit is not in our lookup table for the selected LIVERY
+                        if unknown_list[unit_type] == nil then
+                            unknown_list[unit_type] = true
+                            print("UNKNOWN - " .. unit_type .. " - " .. unit_name)
+                        end
+                    elseif chosen_table[unit_type] ~= nil then -- unit is in our lookup table, and it has a relevant skin
+                        local chosen_livery = chosen_table[unit_type]
+                        if unit_livery ~= chosen_livery then 
+                            unit_data["livery_id"] = chosen_livery 
+                            print("UPDATED - " .. unit_type .. " - " .. unit_name .. " - " .. unit_livery .. " -> " .. chosen_livery)
+                    end
+
+                    else -- unit is in our lookup table, but it doesn't have the desired skin
+                        if unavailable_list[unit_type] == nil then
+                            unavailable_list[unit_type] = true
+                            print("NO-SKIN - " .. unit_type .. " - " .. unit_name)
+                        end
+                    end
+                end
+            end
+        end
+    end
+end
+
+mission_io.write_mission(FILEPATH, mission_data)
---
+++ tools/livery_set.ps1
@@ -0,0 +1,41 @@
+# This script automates copying waypoints from one flight to multiple other flights in a mission file.
+# When run successfully, a new miz file will be created in the same directory as the input miz, named: "mymissionname.wpcopy.miz"
+
+# Usage:
+# Update the below ARGS.
+# miz_file_path: the full path to your *.miz file
+# src_aircraft: the group name of the flight to copy waypoints from
+# dst_aircraft: a space separated list of flight names to copy waypoints to
+
+# ARGS
+$miz_file_path = "C:\Users\Chris\Saved Games\DCS\Missions\test_moose.miz"
+$theme = "desert" # Choose: (desert, winter) ... maybe more later on
+# END OF ARGS
+
+
+
+########
+# Make a tmp miz file, expand it so we can edit it
+########
+# remove any existing tmp file first
+Remove-Item ./tmp_miz.zip -force -ErrorAction SilentlyContinue
+
+Copy-Item $miz_file_path  -Destination ./tmp_miz.zip
+Expand-Archive ./tmp_miz.zip -DestinationPath ./expanded_miz/ -force # we'll reuse this later
+
+
+# run lua script to update the waypoints in the *.miz/mission file
+# updates the ./expanded_miz/mission file
+$cwd = (get-item livery_set.ps1).Directory.FullName # hack to make lua libs work
+./bin/lua/lua54.exe livery_set.lua $cwd/lib ./expanded_miz/mission $theme
+
+##############
+##### zip up the mission files into tmp_miz.zip
+##############
+Compress-Archive -update ./expanded_miz/mission tmp_miz.zip #
+Remove-Item expanded_miz -Recurse -force # delete expanded_miz folder
+
+# rename the zipped up files into a miz file that won't overwrite the original
+$out_file_path = $miz_file_path -replace ".miz$", ".livery.miz"
+Remove-Item $out_file_path -ErrorAction SilentlyContinue # wipe out the previous file, if we ran this before
+Move-Item tmp_miz.zip $out_file_path 
