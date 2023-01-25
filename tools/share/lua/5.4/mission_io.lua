7f7f2ca918121ef87f802fb641381d820635373b
alexgibbs606 <alexgibbs606@gmail.com>
2023-01-25 13:31:59 -0600

    Added method to mission_io to access a missions groups or units

--- tools/share/lua/5.4/mission_io.lua
+++ tools/share/lua/5.4/mission_io.lua
@@ -51,3 +51,104 @@ mission_io.serialize_inner = function(file, o, indent)
         error("cannot serialize a " .. type(o))
     end
 end
+
+
+mission_io.get_groups = function(mission, coalition, country, type)
+	-- Checking optional parameters
+	if mission.mission ~= nil then
+		if type(mission.coalition) == 'string' then coalition = {mission.coalition} else coalition = mission.coalition end
+		if type(mission.country) == 'string' then country = {mission.country} else country = mission.country end
+		if type(mission.type) == 'string' then type = {mission.type} else type = mission.type end
+		mission = mission.mission
+	end
+
+	local groups = {}
+
+	-- Filtering our coalition
+	-- Iterating through our coalitions
+	for _, group_coalition in pairs(mission.coalition) do
+
+		-- Filtering out coalitions we didn't ask for
+		if not x.contains(group_coalition.name, coalition or {'red', 'blue', 'neutrals'}, false) then
+			goto continue_coalition
+		end -- If we didn't filter, we want this group of units
+
+		-- Iterating through each country
+		for _, group_country in pairs(group_coalition.country) do
+
+			-- Filtering out countries we didn't ask for
+			if not x.contains(group_country.name, country or {'usa', 'russia'}, false) then
+				goto continue_country
+			end -- If we didn't filter, we want this country
+
+			-- Iterating through all our units
+			for group_type, group_table in pairs(group_country) do
+
+				-- Since types are stored differe, we can look for the types requested instead of filtering them out
+				if x.contains(group_type, type or {'ship', 'plane', 'static'}, false) then
+
+					-- Iterating through each unit in this group
+					for _, group in pairs(group_table.group) do
+						group.country = group_country.name
+						group.coalition = group_coalition.name
+						group.type = group_type
+
+						-- Adding this unit to the units list
+						table.insert(groups, group)
+					end
+
+				end
+
+			end
+			::continue_country::
+		end
+		::continue_coalition::
+	end
+
+	return groups
+end
+
+
+mission_io.get_units = function(mission, coalition, country, unit_type, group_name)
+	-- Checking optional parameters
+	if mission.mission ~= nil then
+		if type(mission.coalition) == 'string' then coalition = {mission.coalition} else coalition = mission.coalition end
+		if type(mission.country) == 'string' then country = {mission.country} else country = mission.country end
+		if type(mission.type) == 'string' then unit_type = {mission.type} else unit_type = mission.type end
+		if type(mission.groupName) == 'string' then group_name = {mission.groupName} else group_name = mission.groupName end
+		mission = mission.mission
+	end
+
+	local units = {}
+
+	local groups = mission_io.get_groups(mission, coalition, country, unit_type)
+
+	-- If we're not given a group list, then we'll return everything
+	if not group_name then
+		for _, group in pairs(groups) do
+			for _, unit in pairs(group.units) do
+				table.insert(units. unit)
+			end
+		end
+		return units
+	end
+
+	-- If we are asking for a specific group
+	for _, group in pairs(groups) do
+		-- Filtering out any groups we want
+		if not (
+				x.contains(group.name, group_name, false)
+				  or
+				x.contains(group.groupId, group_name, false)
+			) then
+			goto continue
+		end -- If we didn't filter, we want this group
+
+		for _, unit in pairs(group.units) do
+			table.insert(units, unit)
+		end
+
+		::continue::
+	end
+	return units
+end
