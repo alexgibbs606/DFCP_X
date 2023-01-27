92a19a0b566ff305da0c4f6dae55ffc9ead25b85
alexgibbs606 <alexgibbs606@gmail.com>
2023-01-26 23:11:59 -0600

    Added a SQL-like 'where' method for accessing tables

--- tools/share/lua/5.4/tableExtension.lua
+++ tools/share/lua/5.4/tableExtension.lua
@@ -1,12 +1,13 @@
 local inspect = require 'inspect'
 
-
 table.contains = function(refTable, searchField, caseSensitive)
-	-- If our reftable is empty, we return false
-	if refTable == nil then return false end
+    -- Default arguments
+    caseSensitive = caseSensitive or true
 
-	-- By default we're case sensitive
-	caseSensitive = caseSensitive or true
+    -- If our reftable is empty, we return false
+    if refTable == nil then return false end
+    -- Confirming our refTable is a table
+    if type(refTable) ~= 'table' then refTable = {refTable} end
 
     -- If we're case sensitive, we'll run this piece of code
     if type(searchField) == 'string' and caseSensitive then
@@ -31,4 +32,45 @@ table.dump = function(refTable, depth)
 	-- Default depth is 1
 	depth = depth or 1
 	return inspect(refTable, {depth = depth})
+end
+
+
+table.where = function(table_data, search_fields, caseSensitive)
+    --[[ Returns all sub-tables that match the search fields provided.
+
+    This function CAN NOT search for booleans.
+
+    Arguments:
+        table_data: A table that contains tables of data. i.e. a list of tables with accessable data.
+        search_fields: A table mapping fields to values. The field is a table key and the values is a table or value to search for.
+        caseSensitive: Determines if our comparison for values is case sensitive. Defaults to false.
+    --]]
+    -- Handling default arguments
+    caseSensitive = caseSensitive or false
+
+    local out_data = {}
+
+    -- Iterate our data tables, we take it one table at a time
+    for _, data in pairs(table_data) do
+        for field, values in pairs(search_fields) do
+            -- 	Confiming that our value is a table, if it's not, we make it a table
+            local field_value = data[field]
+
+            -- Filtering out data that doesn't match our search params
+            -- Checking for empty check value
+            if field_value == nil then
+                goto failed
+            end
+
+            -- Custom match function
+            if not table.contains(values, field_value, caseSensitive) then
+                goto failed
+            end
+
+        end -- end field/value in search fields
+        -- if we got here, we haven't failed on any field/value pairs
+        table.insert(out_data, data)
+        ::failed::
+    end
+    return out_data
 end
