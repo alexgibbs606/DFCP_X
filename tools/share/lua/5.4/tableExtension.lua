1d6b1e1773b47593e9ededb4071285ed8df91634
alexgibbs606 <alexgibbs606@gmail.com>
2023-01-26 23:10:29 -0600

    Switched padding to tabs

--- tools/share/lua/5.4/tableExtension.lua
+++ tools/share/lua/5.4/tableExtension.lua
@@ -8,20 +8,22 @@ table.contains = function(refTable, searchField, caseSensitive)
 	-- By default we're case sensitive
 	caseSensitive = caseSensitive or true
 
-	-- If we're case sensitive, we'll run this piece of code
-	if caseSensitive then
-		-- Lower once here to improve performance
-		searchField = string.lower(searchField)
+    -- If we're case sensitive, we'll run this piece of code
+    if type(searchField) == 'string' and caseSensitive then
+        -- Lower once here to improve performance
+        searchField = string.lower(searchField)
 
-		for _, value in pairs(refTable) do
-			-- We process our return here to increase performance
-			if searchField == string.lower(value) then return true end
-		end
-		return false
-	end
+        for _, value in pairs(refTable) do
+            -- We process our return here to increase performance
+            if searchField == string.lower(value) then
+                return true
+            end
+        end
+        return false
+    end
 
-	-- If we've gotten here, we're not wanting case sensitive check
-	return refTable[searchField] ~= nil
+    -- If we've gotten here, we're not wanting case sensitive check
+    return refTable[searchField] ~= nil
 end
 
 
